2ca9577baf803a82ede44343558dc70b1898ce34
Wei Zhou <wei.zhou@partner.bmw.com>
2024-08-22 09:28:45 +0800

    fix

--- api/Base.py
+++ api/Base.py
@@ -7,12 +7,12 @@ router = APIRouter(prefix="/v1", tags=["upload"])
 
 @router.post("/upload/", summary="upload files")
 async def _(file: UploadFile = File(...),
-            description=Form(...),
-            title=Form(...)):
+            title=Form(...),
+            description=Form(...)):
     # 读取文件内容
     f = UploadController()
     file = await f.upload_files(file=file,
                                 description=description,
                                 title=title)
-    res = await f.send_files_to_git( title=file["title"],description=file["description"],file_content=file["file_content"])
+    res = await f.send_files_to_git(title=file["title"], description=file["description"], file_content=file["file_content"], file_extension=file["file_extension"])
     return res
--- api/UploadController.py
+++ api/UploadController.py
@@ -1,43 +1,48 @@
 from core.Response import success, fail
 from configs.config import settings
 from fastapi import File, UploadFile, Form
-import httpx,json
+import httpx
+import os
+import json,base64
+
 
 class UploadController:
     def __init__(self):
         self.endpoint = "gitee.com"
         self.owner = "amadeus666"
-        self.repo="Invoice"
-        self.path_prefix="ToBeOrganized/"
-        self.access_token="4fef6165fcdd31f45da7ea514a3aa924"
+        self.repo = "Invoice"
+        self.path_prefix = "ToBeOrganized/"
+        self.access_token = "4fef6165fcdd31f45da7ea514a3aa924"
 
-        
-    async def upload_files(self,file: UploadFile = File(...),
-                       description: str = Form(...),
-                       title: str = Form(...)) -> dict:
+    async def upload_files(self, file: UploadFile = File(...),
+                           title: str = Form(...),
+                           description: str = Form(...)
+                           ) -> dict:
         """
         """
         file_content = await file.read()
-
-        # 构造响应
+        file_content_encoded = base64.b64encode(file_content).decode()
+        filename,file_extension = os.path.splitext(file.filename)
         return {
-            "filename": file.filename,
-            "content_type": file.content_type,
-            "description": description,
             "title": title,
-            "file_content":file_content
+            "description": description,
+            "file_extension": file_extension,
+            "file_content": file_content_encoded
         }
-    async  def send_files_to_git(self, title,description,file_content):
-        path=self.path_prefix+title
 
+    async def send_files_to_git(self, title, description, file_content,file_extension):
+        path = self.path_prefix+title+file_extension
 
         payload_dict = {
             'access_token': self.access_token,
             'content': file_content,
             'message': description
         }
-        
         response = httpx.post(
-            "https://{endpoint}/api/v5/repos/{owner}/{repo}/contents/{path}".format(endpoint=self.endpoint, owner=self.owner, repo=self.repo, path=path), data=payload_dict).text
-        print (response)
-        return response
+            "https://{endpoint}/api/v5/repos/{owner}/{repo}/contents/{path}".format(endpoint=self.endpoint, owner=self.owner, repo=self.repo, path=path), data=payload_dict)
+        if response.status_code != 200:
+            res=fail(msg="Upload failed!")
+        else:
+            res=success("ok",msg="Get env var successfully!")
+        
+        return res
